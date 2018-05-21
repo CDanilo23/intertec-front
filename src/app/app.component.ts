969d43d0bd019994893591459bffdb3b1619f536
CRISTIAN DANILO ORDOÑEZ <CRISTIAN DANILO ORDOEZ@192.168.1.2>
2018-05-20 22:04:36 -0500

    last touches in the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,29 @@ import {IntertecService} from './service/intertec.service';
 export class AppComponent implements OnInit{
   user=' ';
   restrictedWords: Restrictedword[] = [];
-  usedUserNames:  Username[] = []
+  usedUserNames:  Username[] = [];
+  available: any;
+  kindOfList: string;
   
   constructor(private intertecService: IntertecService ){}
   
   ngOnInit(){
     this.user = '';
-    console.log('entro');
   }
 
   validateUserName(){
-    console.log("se fue al servicio el username "+this.user);
-    this.intertecService.getAvailableUserName(this.user);
+    this.restrictedWords = [];
+    this.usedUserNames = [];
+    return this.intertecService.getAvailableUserName(this.user).subscribe(p => {
+      this.available = p.t1;
+      this.kindOfList = p.t3;
+      if(this.available==false && this.kindOfList=='restricted'){
+         this.restrictedWords= p.t2;
+      }
+      else if(this.available==false && this.kindOfList=='not available'){
+        this.usedUserNames= p.t2;
+      }
+    });
 
   }
 }
